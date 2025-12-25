14d84dc50e9d0d478dc8f17625bcffd90519ba44
nico <prananta@swift.ch>
2025-12-25 22:26:15 +0100

    try

--- instrumentation.ts
+++ instrumentation.ts
@@ -2,8 +2,14 @@ export async function register() {
   if (process.env.NEXT_RUNTIME !== "edge") {
     // Dynamic import to avoid edge runtime bundling issues
     console.log("Starting Redis World...");
+    const { createWorld } = await import("@workflow-worlds/redis");
     const { getWorld } = await import("workflow/runtime");
-    await getWorld().start?.();
+    let world = getWorld();
+    if (!world) {
+      // @ts-expect-error - createWorld is not typed
+      world = createWorld();
+    }
+    await world.start?.();
     console.log("Redis World started");
   }
 }
