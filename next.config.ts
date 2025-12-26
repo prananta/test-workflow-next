c5368869e4ac8df6349cc5875209ce186d7f9f94
nico <prananta@swift.ch>
2025-12-26 17:52:17 +0100

    use standalonw

--- next.config.ts
+++ next.config.ts
@@ -2,5 +2,6 @@ import { withWorkflow } from "workflow/next";
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // … rest of your Next.js config
+  output: "standalone",
 };
 export default withWorkflow(nextConfig);
