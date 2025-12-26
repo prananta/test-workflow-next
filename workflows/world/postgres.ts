ca0d00675c87b4bc2f61cae48525cb1631c08417
nico <prananta@swift.ch>
2025-12-26 11:48:45 +0100

    try this

--- workflows/world/postgres.ts
+++ workflows/world/postgres.ts
@@ -1,8 +1,10 @@
 import { createWorld } from "@workflow/world-postgres";
+import { setWorld } from "workflow/runtime";
 
 export const getWorld = () => {
   console.log(`Getting Postgres World...`);
   const world = createWorld();
+  setWorld(world);
   console.log(`Postgres World created`);
 
   return world;
