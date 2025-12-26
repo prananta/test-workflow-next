a170ba99f6a030892f77cfd9fe3c343ac1cc41eb
nico <prananta@swift.ch>
2025-12-26 10:57:25 +0100

    huh

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,7 +1,7 @@
+import { getWorld } from "@/workflows/world/postgres";
+
 export async function register() {
-  // Dynamic import to avoid edge runtime bundling issues
   console.log("Starting Postgres World...");
-  const { getWorld } = await import("workflow/runtime");
   await getWorld().start?.();
   console.log("Postgres World started");
 }
