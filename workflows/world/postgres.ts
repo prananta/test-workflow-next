3aae619902df8f86a30e542adfc002d3535ce2ac
nico <prananta@swift.ch>
2025-12-26 11:42:16 +0100

    try

--- workflows/world/postgres.ts
+++ workflows/world/postgres.ts
@@ -1,12 +1,9 @@
 import { createWorld } from "@workflow/world-postgres";
-import { getWorld as getWorkflowWorld } from "workflow/runtime";
 
 export const getWorld = () => {
   console.log(`Getting Postgres World...`);
-  let world = getWorkflowWorld();
-  if (!world) {
-    world = createWorld();
-    console.log(`Postgres World created`);
-  }
+  const world = createWorld();
+  console.log(`Postgres World created`);
+
   return world;
 };
