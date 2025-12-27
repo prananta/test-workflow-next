a871d38214555d558a58d5b05c2f13a61ce07d5b
nico <prananta@swift.ch>
2025-12-27 15:59:08 +0100

    try tracing

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // … rest of your Next.js config
   output: "standalone",
+  outputFileTracingIncludes: {
+    "/*": ["./node_modules/@workflow/world-postgres/**"],
+  },
 };
 export default withWorkflow(nextConfig);
